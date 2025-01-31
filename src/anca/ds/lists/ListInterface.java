4c089bb73e404c9e112e4e15ae5aedcbdafd09dd
Anca <negulescuanca11@gmail.com>
2025-01-31 20:17:21 +0200

    Add more basic methods to the ListInterface

--- src/anca/ds/lists/ListInterface.java
+++ src/anca/ds/lists/ListInterface.java
@@ -2,8 +2,11 @@ package anca.ds.lists;
 
 public interface ListInterface {
     // void add(int value);
-    // void remove(int value);
+    // void addFirst(int value); // aka prepend
+    // void remove();
+    // void removeFirst();
     // boolean contains(int value);
     int size();
+    // boolean isEmpty();
     void printList();
 }
